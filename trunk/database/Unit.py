a9427a18fdb7a3ef3434a829ac5a57b523429548
dmorton103 <dmorton103@d654c129-cddd-47d4-8472-3d8b74d7a8f8>
2019-08-28 14:46:40 +0000

    more inserts - committing to diff and check quantities

--- trunk/database/Unit.py
+++ trunk/database/Unit.py
@@ -3,6 +3,7 @@ from enum import Enum
 class Unit(Enum):
 	CUP = (1, "cup")
 	FL_OZ = (1/8, "oz")
+	DRY_OZ = (0, "oz")  # not using the conversion factor currently; this wouldn't be uniform across dry ingredients
 	TBSP = (1/16, "tbsp")
 	TSP = (1/(16*3), "tsp")
 
