782d1959103489e30dc56690365f4d8c1b12164b
dmorton103 <dmorton103@d654c129-cddd-47d4-8472-3d8b74d7a8f8>
2019-09-05 00:28:18 +0000

    More ingredients

--- trunk/py/database/Ingredients.py
+++ trunk/py/database/Ingredients.py
@@ -4,12 +4,27 @@ from py.database.Models import Ingredient
 
 """
 These are just some shortcuts for common ingredients
+They actually do help as far as 
 """
+
+
+def Egg(quantity: float):
+    return Ingredient("Egg", quantity)
+
 
 # region Butter and oils
 
 def Butter(quantity: Optional[float], unit_of_measure="", *, does_scale=True):
     return Ingredient("Butter", quantity, unit_of_measure, does_scale)
+
+def Margarine(quantity: Optional[float], unit_of_measure="", *, does_scale=True):
+    return Ingredient("Margarine", quantity, unit_of_measure, does_scale)
+
+def CanolaOil(quantity: Optional[float], unit_of_measure="", *, does_scale=True):
+    return Ingredient("Canola oil", quantity, unit_of_measure, does_scale)
+
+def OliveOil(quantity: Optional[float], unit_of_measure="", *, does_scale=True):
+    return Ingredient("Olive oil", quantity, unit_of_measure, does_scale)
 
 # endregion
 
@@ -21,6 +36,9 @@ def AlmondMilk(quantity: Optional[float], unit_of_measure="", *, does_scale=True
 
 def Milk(quantity: Optional[float], unit_of_measure="", *, does_scale=True):
     return Ingredient("Milk", quantity, unit_of_measure, does_scale)
+
+def VegetableBroth(quantity: Optional[float], unit_of_measure="", *, does_scale=True):
+    return Ingredient("Vegetable broth", quantity, unit_of_measure, does_scale)
 
 # endregion
 
@@ -49,17 +67,28 @@ def BakingPowder(quantity: Optional[float], unit_of_measure="", *, does_scale=Tr
 def BakingSoda(quantity: Optional[float], unit_of_measure="", *, does_scale=True):
     return Ingredient("Baking soda", quantity, unit_of_measure, does_scale)
 
+def Basil(quantity: Optional[float], unit_of_measure="", *, does_scale=True):
+    return Ingredient("Basil", quantity, unit_of_measure, does_scale)
+
+def Cilantro(quantity: Optional[float], unit_of_measure="", *, does_scale=True):
+    return Ingredient("Cilantro", quantity, unit_of_measure, does_scale)
+
 def Cinnamon(quantity: Optional[float], unit_of_measure="", *, does_scale=True):
     return Ingredient("Cinnamon", quantity, unit_of_measure, does_scale)
 
+def Pepper(quantity: Optional[float], unit_of_measure="", *, does_scale=True):
+    return Ingredient("Pepper", quantity, unit_of_measure, does_scale)
+
 def Salt(quantity: Optional[float], unit_of_measure="", *, does_scale=True):
     return Ingredient("Salt", quantity, unit_of_measure, does_scale)
-
 
 # endregion
 
 
 # region Sugar and other sweet things
+
+def ChocolateChips(quantity: Optional[float], unit_of_measure="", *, does_scale=True):
+    return Ingredient("Chocolate chips", quantity, unit_of_measure, does_scale)
 
 def BrownSugar(quantity: Optional[float], unit_of_measure="", *, does_scale=True):
     return Ingredient("Brown sugar", quantity, unit_of_measure, does_scale)
