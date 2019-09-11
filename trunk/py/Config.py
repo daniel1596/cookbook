7b08a8f89c80c7575c285d9d9ea3165f1f3d1418
dmorton103 <dmorton103@d654c129-cddd-47d4-8472-3d8b74d7a8f8>
2019-09-11 17:12:13 +0000

    Setting the Config object to no longer be an enum but instead an extendable class. Created ProductionConfig and DevelopmentConfig classes that toggle if vue.js or vue.min.js is used.

--- trunk/py/Config.py
+++ trunk/py/Config.py
@@ -1,4 +1,26 @@
-from enum import Enum
+from abc import abstractmethod
 
-class Config(Enum):
-    DB_LOCATION = 'sqlite:///db/recipes.db?check_same_thread=False'
+
+class ConfigBase:  # not inheriting from Enum because you can't extend the Enum class
+    database_location = 'sqlite:///db/recipes.db?check_same_thread=False'
+
+    @property
+    @abstractmethod
+    def vue_file(self) -> str:
+        pass
+
+
+class DevelopmentConfig(ConfigBase):
+    @property
+    def vue_file(self) -> str:
+        return "vue.js"
+
+
+class ProductionConfig(ConfigBase):
+    @property
+    def vue_file(self) -> str:
+        return "vue.min.js"
+
+
+# global object for imports
+Config = DevelopmentConfig()
