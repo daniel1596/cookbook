e1822e00ffa11ccfdd59eef267f1cd5209c17c0c
dmorton103 <dmorton103@d654c129-cddd-47d4-8472-3d8b74d7a8f8>
2019-09-11 17:29:29 +0000

    Spaces to tabs, globally (didn't realize when I reinstalled PyCharm on my work PC that it would to 4 spaces instead of tabs)

--- trunk/py/database/Main.py
+++ trunk/py/database/Main.py
@@ -5,21 +5,21 @@ from py.database.Models import Recipe, Ingredient
 
 
 def get_all_ingredients() -> List[Ingredient]:
-    return Ingredient.query.all()
+	return Ingredient.query.all()
 
 
 def get_all_recipes() -> List[Recipe]:
-    return Recipe.query.order_by(Recipe.Name).all()
+	return Recipe.query.order_by(Recipe.Name).all()
 
 
 def get_recipe_by_name(name: str) -> Recipe:
-    recipe = Recipe.query\
-        .options(joinedload(Recipe.Ingredients))\
-        .filter(Recipe.Name.like(f"%{name}%"))\
-        .first()  # I am guessing the filtering must be case-insensitive by default
+	recipe = Recipe.query\
+		.options(joinedload(Recipe.Ingredients))\
+		.filter(Recipe.Name.like(f"%{name}%"))\
+		.first()  # I am guessing the filtering must be case-insensitive by default
 
-    # For unknown reasons, SQLAlchemy is auto-sorting the list by ingredient name, it seems like
-    #  so I have to do this to get around that
-    recipe.Ingredients.sort(key=lambda ingredient: ingredient.IngredientID)
+	# For unknown reasons, SQLAlchemy is auto-sorting the list by ingredient name, it seems like
+	#  so I have to do this to get around that
+	recipe.Ingredients.sort(key=lambda ingredient: ingredient.IngredientID)
 
-    return recipe
+	return recipe
