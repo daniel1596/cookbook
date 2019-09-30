6bd69ea7084eae8466c7b28d8be8c9e5150ca157
dmorton103 <dmorton103@d654c129-cddd-47d4-8472-3d8b74d7a8f8>
2019-09-30 14:11:50 +0000

    Fix recipe I might make soon

--- trunk/py/database/Queries.py
+++ trunk/py/database/Queries.py
@@ -13,10 +13,15 @@ def get_all_recipes() -> List[Recipe]:
 
 
 def get_recipe_by_name(name: str) -> Recipe:
+	# I think the query below is fine, but note that if someone were to hit /recipes/granola, they would get
+	# the first granola recipe in the collection as a detail page, since I'm doing a SQL check for LIKE '%granola%'
+	# Granted, the way the app is set up, the above should never happen (the app guides you to the proper link), but still.
+	
 	recipe = Recipe.query\
 		.options(joinedload(Recipe.Ingredients))\
 		.filter(Recipe.Name.like(f"%{name}%"))\
 		.first()  # I am guessing the filtering must be case-insensitive by default
+	
 
 	# For unknown reasons, SQLAlchemy is auto-sorting the list by ingredient name, it seems like
 	#  so I have to do this to get around that
